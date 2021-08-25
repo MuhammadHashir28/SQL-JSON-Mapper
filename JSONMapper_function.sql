af2eac3b60f6e484d28cf1db601ec9e70947ed7d
Muhammad Hashir <58470274+MuhammadHashir28@users.noreply.github.com>
2021-08-25 19:26:34 +0500

    Added SQL Files

---
+++ JSONMapper_function.sql
@@ -0,0 +1,177 @@
+
+
+/****** Object:  UserDefinedFunction [EDE].[JSONOBJMapper_V2]    Script Date: 8/25/2021 7:07:09 PM ******/
+SET ansi_nulls ON
+
+go
+
+SET quoted_identifier ON
+
+go
+
+CREATE FUNCTION [EDE].[JsonMapper] (     @JSON VARCHAR(max),
+                                         @Path    VARCHAR(max),
+                                         @SetValue   VARCHAR(max),
+                                         @type    VARCHAR(max)
+										 )
+
+returns VARCHAR(max)
+AS
+  BEGIN
+
+
+      SET @Path=Replace(@Path, '$.', '');
+      SET @Path=Replace(@Path, '"', '');
+      SET @SetValue=Cast(@SetValue AS VARCHAR(max))
+
+      DECLARE @delimiter NVARCHAR(1);
+      DECLARE @out_put VARCHAR(max);
+
+      SET @delimiter = '.';
+
+      BEGIN
+          DECLARE @SetValue_SMLL NVARCHAR(max),
+                  @pos        INT= 0,
+                  @len        INT= 0;
+
+          SET @Path = CASE
+                        WHEN RIGHT(@Path, 1) != @delimiter THEN @Path +
+                        @delimiter
+                        ELSE @Path
+                      END;
+
+          DECLARE @Flag INT;
+
+          SET @Flag = 0;
+
+          WHILE Charindex(@delimiter, @Path, @pos + 1) > 0
+            BEGIN
+                SET @len = Charindex(@delimiter, @Path, @pos + 1) - @pos;
+                SET @SetValue_SMLL = Substring(@Path, @pos, @len);
+
+                IF( @Flag > 0 )
+                  BEGIN
+                      SET @out_put = @out_put + '."' + Ltrim(Rtrim(@SetValue_SMLL))
+                                     +
+                                     '"'
+                      ;
+                  END;
+                ELSE
+                  BEGIN
+                      SET @out_put = '"' + Ltrim(Rtrim(@SetValue_SMLL)) + '"';
+                  END;
+
+                IF NOT EXISTS(SELECT *
+                              FROM   Openjson(@JSON, '$.' + @out_put))
+                  BEGIN
+                      DECLARE @EMPTY VARCHAR(max);
+
+                      SET @EMPTY='{}'
+                      SET @JSON=Json_modify(@JSON, '$.' + @out_put,
+                                   Json_query('{}')
+                                   );
+                  END
+
+                SET @Flag = @Flag + 1;
+                SET @pos = Charindex(@delimiter, @Path, @pos+@len) + 1;
+
+                IF( Len(@Path) + 1 = @pos )
+                  BEGIN
+                      IF( @SetValue IS NULL
+                           OR @SetValue = 'null' )
+                        BEGIN
+                            SET @JSON=Json_modify(@JSON, '$.' + @out_put,
+                                         'null'
+                                         )
+                        END
+                      ELSE IF( @SetValue LIKE '%{%'
+                          AND @SetValue LIKE '%}%' )
+                        BEGIN
+                            SET @JSON=Json_modify(@JSON, '$.' + @out_put,
+                                         Json_query(@SetValue)
+                                         )
+                        END
+                      ELSE
+                        BEGIN
+                            IF( ( @SetValue = '1' )
+                                 OR ( @SetValue = 'true' ) )
+                              AND @type LIKE '%boolean%'
+                              BEGIN
+                                  SET @JSON=Json_modify(@JSON, '$.' +
+                                               @out_put,
+                                               Cast(1 AS BIT))
+                              END
+                            ELSE IF( @SetValue = '0' )
+                               OR ( @SetValue = 'false' )
+                                  AND @type LIKE '%boolean%'
+                              BEGIN
+                                  SET @JSON=Json_modify(@JSON, '$.' +
+                                               @out_put,
+                                               Cast(0 AS BIT))
+                              END
+                            ELSE IF ( Isnumeric(@SetValue) = 1 )
+                               AND @type LIKE '%numeric%'
+                              BEGIN
+                                  IF( Isnumeric(Replace(Replace(@SetValue, '+', 'A'
+                                                ),
+                                                '-'
+                                                ,
+                                                'A')
+                                                + '.0e0') = 1 )
+                                    BEGIN
+                                        SET @JSON=Json_modify(@JSON, '$.'
+                                                     +
+                                                     @out_put
+                                                     ,
+                                                     Cast
+                                                     (
+                                                     ( @SetValue ) AS BIGINT))
+                                    END
+                                  ELSE
+                                    BEGIN
+                                        SET @JSON=Json_modify(@JSON, '$.'
+                                                     +
+                                                     @out_put
+                                                     ,
+                                                     Cast
+                                                     (
+                                                     ( @SetValue ) AS
+                                                     DECIMAL(18, 2))
+                                                     )
+                                    END
+                              END
+                            ELSE IF( @SetValue = '[]' )
+                              AND @type LIKE '%array%'
+                              BEGIN
+                                  SET @JSON=Json_modify(@JSON, '$.' +
+                                               @out_put,
+                                               Json_query('[]')
+                                               )
+                              END
+                            ELSE IF @type IS NULL
+                                OR @type LIKE '%string%'
+                              BEGIN
+                                  SET @JSON=Json_modify(@JSON, '$.' +
+                                               @out_put,
+                                               (
+                                               @SetValue )
+                                               )
+                              END
+                            ELSE IF @type IS NULL
+                                OR @type LIKE '%object%'
+                              BEGIN
+                                  SET @JSON=Json_modify(@JSON, '$.' +
+                                               @out_put,
+                                               Json_query(@SetValue)
+                                               )
+                              END
+                        END
+                  END
+            --PRINT @pos
+            END;
+      END
+
+      SET @JSON=Replace(@JSON, '"null"', 'null')
+
+      RETURN @JSON;
+  END  
---
+++ Readme.md
@@ -0,0 +1,57 @@
+# Gaming Blog Website
+
+A multi-page game website built using `HTML` and `CSS` skills which includes custom images, layout, and styling.
+
+Open and view the Project using the `.zip` file provided or at my [Github Repository](https://github.com/MuhammadHashir28/Game-blog-website).
+
+## Table of Contents
+- [Getting Started](#getting-started)
+	- [Tools Required](#tools-required)
+	- [Installation](#installation)
+- [Development](#development)
+- [Features](#features)
+- [Running the App](#running-the-app)
+
+
+## Getting Started
+
+This project was built from scratch using HTML,CSS only with picture credit from 'https://www.warnerbros.com/games-and-apps/batman-arkham-knight'
+
+### Tools Required
+
+No additional tools are required apart from a text editor of your choice
+
+### Installation
+
+No additional installation is required for this project
+
+## Development
+
+* Create `index.html` as the landing page with the following:
+  * Navbar with links to other pages
+  * Cover image 
+  * Basic page content
+  * Footer with social media links
+  
+* Create `views` directory with the following pages:
+  * `about.html` with about-us info.
+  * `contact.html` with contact details.
+  * `watch.html` with trailer link attached.
+  
+  Reuse navbar and footer code
+
+* Create `css` directory with the following files:
+  * `sample.css` with the global styling
+  
+For details now how everything has been implemented, refer the source code.
+
+## Features
+
+* Only first blog post has a dedicated page for it, rest are all dummies.
+* There are 2 major pages for the website: `Landing page` and `Watch Page`.
+* The project also contains a dedicated `About-Us` page.
+
+## Running the App
+
+* Open the project through the `.zip` file provided and extract the files. 
+  > Open `index.html` in the browser of your choice.
